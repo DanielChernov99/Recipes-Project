74560178c00371567eb9784fe8a3fa478363f2d9
Daniel Chernov <chernov.dani@gmail.com>
2026-07-23 11:12:50 +0300

    Implement readRecipes and wrtieRecipes functions for recipe data handling

--- src/models/recipeModel.js
+++ src/models/recipeModel.js
@@ -4,6 +4,15 @@ const { v4: uuidv4 } = require("uuid");
 
 const recipesPath = path.join(__dirname, "../data/recipes.json");
 
+async function readRecipes() {
+  const data = await fs.promises.readFile(recipesPath, "utf-8");
+  return JSON.parse(data);
+}
+
+async function wrtieRecipes(recipes) {
+  await fs.promises.writeFile(recipesPath, JSON.stringify(recipes, null, 2));
+}
+
 async function getRecipes(query = {}) {}
 
 async function getRecipeById(id) {}
